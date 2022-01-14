cd402a48e9241bad7604a334a0ac83e116581322
bose-dev360 <bosedev360@outlook.com>
2022-01-14 03:44:07 -0500

    Updates

---
+++ precache-manifest.7273c1d0964519eda13d855320facb51.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8c293574ef34bc7d47c",
+    "url": "/resume/static/css/main.8631d4c5.chunk.css"
+  },
+  {
+    "revision": "a8c293574ef34bc7d47c",
+    "url": "/resume/static/js/main.145fae21.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "9ec6e0cd7c15b9ebb3c5",
+    "url": "/resume/static/css/2.801ae4e0.chunk.css"
+  },
+  {
+    "revision": "9ec6e0cd7c15b9ebb3c5",
+    "url": "/resume/static/js/2.11962201.chunk.js"
+  },
+  {
+    "revision": "686c6799af89d7e925863f2048753d18",
+    "url": "/resume/static/media/self.686c6799.jpg"
+  },
+  {
+    "revision": "563591ad9577eb6060c41ee6343ffd38",
+    "url": "/resume/static/media/001.563591ad.png"
+  },
+  {
+    "revision": "e1e93b8366457979d6c4be8f76482469",
+    "url": "/resume/static/media/002.e1e93b83.png"
+  },
+  {
+    "revision": "049b87d24f649c200da012cd8cfa3801",
+    "url": "/resume/static/media/003.049b87d2.png"
+  },
+  {
+    "revision": "67edcff8f089d9f2040bae3eb463d29e",
+    "url": "/resume/static/media/demo.67edcff8.svg"
+  },
+  {
+    "revision": "583bdea184735edc7c884e9afd7b381d",
+    "url": "/resume/index.html"
+  }
+];
